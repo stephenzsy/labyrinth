0259a66a16a0c09c5ce91722f3c9014d12f84dd4
Stephen Zhang <stephen.zsy@gmail.com>
2014-03-28 23:47:51 -0700

    new rails site

---
+++ daedalus/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Daedalus::Application.config.secret_key_base = 'fa564eb8fc76519948be83944013574a77a361b4e824a06099300fb7685ca55981f6ab326b8db773b9a3f46ccfc59d5b32f7bd839c15c98c9351fa3c304bf887'
