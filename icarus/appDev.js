a06dc63172660c9000017cd3110c322692b45f4b
Stephen Zhang <stephen.zsy@gmail.com>
2014-06-03 00:23:01 -0700

    added updated app.js

--- icarus/appDev.js
+++ icarus/appDev.js
@@ -4,7 +4,6 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var log = require('log4js').getLogger();
 
 var routes = require('./routes/index');
 var bootstrap = require('./routes/bootstrap');
@@ -28,7 +27,7 @@ app.use('/bootstrap', bootstrap);
 app.use('/api', api);
 
 /// catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -39,7 +38,7 @@ app.use(function (req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
+    app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -50,16 +49,12 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
         error: {}
     });
-});
-
-var server = app.listen(4000, function () {
-    log.info('Express server listening on port ' + 4000);
 });
 
 
