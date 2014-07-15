4bd8bd09ff174dbf3228053c68fbface9564cd23
Stephen Zhang <stephen.zsy@gmail.com>
2014-07-14 23:43:18 -0700

    0.09

--- icarus/app.js
+++ icarus/app.js
@@ -28,6 +28,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //app.use('/', routes);
+app.use(cors());
 app.use('/', icarus);
 app.use('/bootstrap', bootstrap);
 app.use('/api', api);
@@ -36,7 +37,6 @@ app.use('/admin/bootstrap', adminBootstrap);
 app.use('/admin/instances', adminInstances);
 app.use('/status', require('./routes/status'));
 
-app.options('*', cors());
 
 /// catch 404 and forward to error handler
 app.use(function (req, res, next) {
